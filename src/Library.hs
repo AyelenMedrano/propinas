8f869d4005b6afce24ed5ed2c3873a1040607803
AyelenMedrano <ayelen316@gmail.com>
2021-04-29 11:45:42 -0300

    Mi version del ejercicio propinas

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,45 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Nombre = String
+type Propina = Number
+
+
+--Version 1
+--cuantoPagar :: Number -> Nombre -> String -> Propina
+--cuantoPagar costoComida "recomendada" _ = costoComida + criterioPropinaRecomendada costoComida 
+--cuantoPagar costoComida "MrPink" _ = costoComida + criterioPropinaMrPink costoComida 
+--cuantoPagar costoComida "conservador" _ = costoComida + criterioPropinaConservador costoComida 
+--cuantoPagar costoComida "TioCarlos" mozo = costoComida + criterioPropinaSegunNombre costoComida mozo
+
+--Version 2
+cuantoPagar costoComida criterioPropina = costoComida + criterioPropina costoComida
+
+criterioPropinaSegunNombre :: String -> Number -> Propina
+criterioPropinaSegunNombre mozo costoComida | "Carlos" == mozo = ((*2) . criterioPropinaRecomendada) costoComida
+                                            | otherwise = ((/2) . criterioPropinaRecomendada) costoComida
+
+criterioPropinaMrPink :: Number -> Propina
+criterioPropinaMrPink costoComida  = 0
+
+criterioPropinaConservador :: Number -> Propina
+criterioPropinaConservador costoComida  | esImpar costoComida = 25
+                                        | otherwise = 20
+
+criterioPropinaRecomendada :: Number -> Propina
+criterioPropinaRecomendada  = (* 0.1)
+
+esImpar :: Number -> Bool
+esImpar = (odd . truncate)
+
+--ALGO INTERESANTE EN LA FUNCION criterioPropinaSegunNombre
+-- nos damos cuenta que le falta un parametro, el nombre del mozo
+-- para arreglar esto el secreto esta en "crear" otra funcion ya con el parametro del nombre del mozo, cuando consulto
+-- Por ejemplo, se consulta de esta manera:
+-- *Spec Library Spec> cuantoPagar 600 (criterioPropinaSegunNombre "Carlos")
+-- 700
+-- Vemos que FUNCIONO!!!! Y nos dio un resultado correcto, en cambio si consulto esto:
+-- *Spec Library Spec> cuantoPagar 600 criterioPropinaSegunNombre 
+-- Me va a dar un ERROR de falta de parametro
+
+--RECORDA LA VERSION 2 ES LA QUE DEBO UTILIZAR, EN LA VERSION 1 MATO GATITOS :c
